1e608b49f5ede30ed7e6df4bed1b955bedc38e88
Suraj Gupta <82599987+surajm7@users.noreply.github.com>
2024-10-06 16:24:11 +0530

    add shadcn ui button

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import { Button } from './components/ui/button';
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Welcome to Bookstore admin dashboard</h1>
+     <Button> Click Me</Button>
+    </>
+  )
+}
+
+export default App
